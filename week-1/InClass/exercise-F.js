d41441e23b357cb0a18d9d028aabf352b7eeaa9b
juandesousa <juan.cdsb@gmail.com>
2021-06-29 21:14:56 +0200

    inclass

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,10 @@
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+
+const totalStudents=`Number of students: ${numberOfStudents}`;
+const totalMentors=`Number of mentors: ${numberOfMentors}`;
+const totalStudentsMentors=`Total number of students and mentors: ${numberOfStudents+numberOfMentors}`;
+
+console.log(totalStudents);
+console.log(totalMentors);
+console.log(totalStudentsMentors);
---
+++ week-1/InClass/exercise-G.js
@@ -0,0 +1,11 @@
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const total=numberOfStudents+numberOfMentors;
+
+
+const totalStudents=`Percentage students: ${Math.round(numberOfStudents*100/23)}%`;
+const totalMentors=`Percentage mentors: ${Math.round(numberOfMentors*100/23)}%`;
+
+console.log(totalStudents);
+console.log(totalMentors);
+
