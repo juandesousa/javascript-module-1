e4a382662af1357d70d08a6c12f5c8ef48986a84
juandesousa <juan.cdsb@gmail.com>
2021-06-29 20:29:46 +0200

    Exercise E

---
+++ week-1/InClass/exercise-E.js
@@ -0,0 +1,9 @@
+const  saludo = 'Hola';
+const  nombre = 'Juan';
+
+const concatenado = saludo+" "+nombre;
+
+const interpolacion = `${saludo} ${nombre}`;
+
+console.log('Concatenado:', concatenado);
+console.log('Interpolacion:', interpolacion);
