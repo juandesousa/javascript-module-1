b7a438eca36197782b3dd82f2eaa4c94f1bcbe4d
juandesousa <juan.cdsb@gmail.com>
2021-07-17 11:59:05 +0200

    homework

--- week-3/Homework/C-array-every/exercise.js
+++ week-3/Homework/C-array-every/exercise.js
@@ -5,7 +5,7 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents = group.every( member => students.includes(member)); // complete this statement
+var groupIsOnlyStudents = group.every( member => students.includes(member))//? console.log("The group contains only students") : console.log("The group does not contain only students"); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
